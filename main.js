92356e4379aae4d4b2139a1b5aa40bf58d6a98c4
Tyler Potts <tylerpottsdev@gmail.com>
2020-10-21 12:21:44 +0100

    first

---
+++ main.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+  const anchors = document.querySelectorAll('a');
+  const transition_el = document.querySelector('.transition');
+
+  setTimeout(() => {
+    transition_el.classList.remove('is-active');
+  }, 500);
+
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      let target = e.target.href;
+
+      console.log(transition_el);
+
+      transition_el.classList.add('is-active');
+
+      console.log(transition_el);
+
+      setInterval(() => {
+        window.location.href = target;
+      }, 500);
+    })
+  }
+}
